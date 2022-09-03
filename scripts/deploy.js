bb52e11eb5faf32ef444fe29db39612fda73be73
kihiuFrank <franklinekihiu@gmail.com>
2022-09-03 20:15:29 +0300

    Contract Verification on Etherscan

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,53 @@
 //imports
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 //fuction
 
 async function main() {
+    //DEPLOYING CONTRACT
     const SimpleStorageFactory = await ethers.getContractFactory(
         "SimpleStorage"
     )
     console.log("Deploying Contract...")
     const simpleStorage = await SimpleStorageFactory.deploy()
     await simpleStorage.deployed()
-
     console.log(`Deployed Contract to: ${simpleStorage.address}`)
+
+    // VERIFING CONTRACT (if we are on a TestNet)
+    //What happens if we deploy to our hardhat network?  We shouldn't call verify()
+    // we use chainId as the solution
+    if (network.config.chainId === 4 && process.env.ETHERSCAN_API_KEY) {
+        console.log("Waiting for blocks txes...")
+        await simpleStorage.deployTransaction.wait(6)
+        await verify(simpleStorage.address, [])
+    }
+
+    // INTERATING WITH THE CONTRACT
+    //get current value
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`Current Value is: ${currentValue}`)
+
+    //Update Current Value
+    const transactionResponse = await simpleStorage.store(5)
+    await transactionResponse.wait(1)
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`Updated Value is: ${updatedValue}`)
+}
+
+async function verify(contractAddress, args) {
+    console.log("Verifying Contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified!")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 //funtion call
